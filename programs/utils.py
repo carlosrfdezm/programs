129a83e2f01dbd9bc20cf9e86be248d4a5caf3a6
Boris Pérez <polux27@gmail.com>
2020-03-27 12:08:44 -0400

    Agregando vosta de error

--- programs/utils.py
+++ programs/utils.py
@@ -3,10 +3,17 @@ from programs.models import ProgramMember
 
 def user_is_program_cs(user, program):
     try:
-        member=ProgramMember.objects.get(user=user)
+        member=ProgramMember.objects.get(user=user, program=program)
         if member.role=='Coordinador' or member.role=='Secretario':
             return True
         else:
             return False
+    except:
+        return False
+
+def user_is_program_member(user, program):
+    try:
+        member=ProgramMember.objects.get(user=user, program=program)
+        return True
     except:
         return False
--- programs/views.py
+++ programs/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse
 from django.utils.text import slugify, phone2numeric
 
 from programs.models import Program, ProgramInitRequirements, PhdStudent, Student, StudentInitRequirement, ProgramMember
-from programs.utils import user_is_program_cs
+from programs.utils import user_is_program_cs, user_is_program_member
 
 
 def index(request, program_slug):
@@ -42,88 +42,93 @@ def home(request, program_slug):
 @login_required
 def create_student(request, program_slug):
     program=Program.objects.get(slug=program_slug)
-    if request.method == 'POST':
-        try:
-            user= User.objects.get(email=request.POST['student_email'])
-        except:
-            passwd = program_slug + str(random.randint(1000000, 9999999))
-            user = User.objects.create_user(
-                slugify(request.POST['student_email'].split('@')[0]),
-                request.POST['student_email'],
-                '12345678',  # Cambiar despues por contrase;a generada
+    if user_is_program_cs(request.user, program):
+        if request.method == 'POST':
+            try:
+                user = User.objects.get(email=request.POST['student_email'])
+            except:
+                passwd = program_slug + str(random.randint(1000000, 9999999))
+                user = User.objects.create_user(
+                    slugify(request.POST['student_email'].split('@')[0]),
+                    request.POST['student_email'],
+                    '12345678',  # Cambiar despues por contrase;a generada
 
+                )
+                user.first_name = request.POST['student_name']
+                user.last_name = request.POST['student_surename']
+                user.save()
+
+            student = Student(
+                user=user,
+                program=program,
             )
-            user.first_name = request.POST['student_name']
-            user.last_name = request.POST['student_surename']
-            user.save()
+            student.save()
 
-        student = Student(
-            user=user,
-            program=program,
-        )
-        student.save()
+            new_student = PhdStudent(
+                student=student,
+                status='solicitante',
+            )
+            new_student.save()
 
-        new_student=PhdStudent(
-            student=student,
-            status='solicitante',
-        )
-        new_student.save()
+            for requirement in ProgramInitRequirements.objects.filter(program=program):
 
-        for requirement in ProgramInitRequirements.objects.filter(program=program):
+                if 'student_requirement_' + str(requirement.id) in request.POST:
+                    new_student_requirement = StudentInitRequirement(
+                        student=student,
+                        requirement=requirement,
+                        accomplished=True,
+                    )
+                    new_student_requirement.save()
 
-            if 'student_requirement_' + str(requirement.id) in request.POST:
-                new_student_requirement=StudentInitRequirement(
-                    student=student,
-                    requirement=requirement,
-                    accomplished=True,
-                )
-                new_student_requirement.save()
+                else:
+                    new_student_requirement = StudentInitRequirement(
+                        student=student,
+                        requirement=requirement,
+                    )
+                    new_student_requirement.save()
 
-            else:
-                new_student_requirement = StudentInitRequirement(
-                    student=student,
-                    requirement=requirement,
-                )
-                new_student_requirement.save()
-
-
-        return HttpResponseRedirect(reverse('programs:students_list', args=[program_slug]))
-    else:
-        context={
-            'program':program,
-            'init_requirements':ProgramInitRequirements.objects.filter(program=program)
-        }
-        if Program.objects.get(slug=program_slug).type == 'phd':
-            return render(request,'programs/create_phd_student.html', context)
+            return HttpResponseRedirect(reverse('programs:students_list', args=[program_slug]))
         else:
-            return HttpResponse('El programa no es un doctorado')
+            context = {
+                'program': program,
+                'init_requirements': ProgramInitRequirements.objects.filter(program=program)
+            }
+            if Program.objects.get(slug=program_slug).type == 'phd':
+                return render(request, 'programs/create_phd_student.html', context)
+            else:
+                return HttpResponse('El programa no es un doctorado')
+    else:
+        return HttpResponse('Error, acceso solo a coordinadores y secretarios')
 
 
 @login_required
 def students_list(request, program_slug, scope):
     program=Program.objects.get(slug=program_slug)
-    if scope == 'all':
-        context={
-            'program': program,
-            'students': Student.objects.filter(program=program),
-        }
-    elif scope == 'requesters':
-        context={
-            'program': program,
-            'students': Student.objects.filter(program=program, phdstudent__status='solicitante'),
-        }
-    elif scope == 'aproved':
-        context = {
-            'program': program,
-            'students': Student.objects.filter(program=program, phdstudent__status = 'doctorando'),
-        }
-    elif scope == 'graduated':
-        context = {
-            'program': program,
-            'students': Student.objects.filter(program=program, phdstudent__status = 'graduado'),
-        }
+    if user_is_program_member(request.user, program):
+        if scope == 'all':
+            context = {
+                'program': program,
+                'students': Student.objects.filter(program=program),
+            }
+        elif scope == 'requesters':
+            context = {
+                'program': program,
+                'students': Student.objects.filter(program=program, phdstudent__status='solicitante'),
+            }
+        elif scope == 'aproved':
+            context = {
+                'program': program,
+                'students': Student.objects.filter(program=program, phdstudent__status='doctorando'),
+            }
+        elif scope == 'graduated':
+            context = {
+                'program': program,
+                'students': Student.objects.filter(program=program, phdstudent__status='graduado'),
+            }
 
-    return render(request, 'programs/students_list.html', context)
+        return render(request, 'programs/students_list.html', context)
+    else:
+        return error_500(request, program, 'Usted no tiene acceso a esta página')
 
 @login_required
 def edit_student(request, program_slug, student_id):
@@ -138,8 +143,12 @@ def edit_student(request, program_slug, student_id):
 
             Student.objects.filter(pk=student_id).update(
                 phone=request.POST['student_phone'],
-                country=request.POST['student_country']
+                country=request.POST['student_country'],
+
                             )
+            PhdStudent.objects.filter(student=Student.objects.get(pk=student_id)).update(
+                status=request.POST['student_status']
+            )
             try:
                 if request.FILES['student_picture']:
                     Student.objects.filter(pk=student_id).update(
@@ -168,6 +177,12 @@ def edit_student(request, program_slug, student_id):
             }
             return render(request, 'programs/edit_phd_student.html', context)
     else:
-        return HttpResponse('Error: Usuario no CS')
+        return error_500(request,program,'Usted no tiene privilegios para editar estudiantes de este programa.')
 
 
+def error_500(request, program, error_message):
+    context={
+        'program':program,
+        'error_message':error_message,
+    }
+    return render(request,'programs/error_500.html', context)
