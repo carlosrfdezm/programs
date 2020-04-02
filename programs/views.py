90c6b33b7f0a4ead080d55d2bd592c9693b59005
Boris Pérez <polux27@gmail.com>
2020-04-02 13:34:38 -0400

    Mostrando tb los ingresos al programa en la grafica de area

--- programs/views.py
+++ programs/views.py
@@ -664,6 +664,8 @@ def ajx_this_year_requests(request, program_slug):
     response_data=[]
     labels = []
     data = []
+    data_1=[]
+    data_2=[]
     meses = {1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio", 7: "Julio",
              8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"}
 
@@ -671,7 +673,11 @@ def ajx_this_year_requests(request, program_slug):
 
     for i in range(1,now().month+1):
         labels.append(meses[i])
-        data.append(Student.objects.filter(request_date__year=now().year,request_date__month=i).__len__())
+        data_1.append(Student.objects.filter(request_date__year=now().year,request_date__month=i).__len__())
+        data_2.append(Student.objects.filter(init_date__year=now().year,init_date__month=i).__len__())
+
+    data.append(data_1)
+    data.append(data_2)
 
     response_data.append(labels)
     response_data.append(data)
