6704aa3f3a5fdcd10965c61fedf4cec36935afce
Boris Pérez <polux27@gmail.com>
2020-04-04 14:11:02 -0400

    Resuelto lo de actualizar contraseña

--- programas/urls.py
+++ programas/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+path('password_reset/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('accounts/password_reset/done/', auth_views.PasswordResetDoneView().as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('admin/', admin.site.urls),
     path('', include('log.urls', namespace='log')),
     path('<str:program_slug>/', include('programs.urls', namespace='programs')),
+
 ]
