dbdb0906f3da374fc73974d492553b9829823a3e
Boris Perez <boris_perez@unah.edu.cu>
2020-08-31 09:28:14 -0400

    Si no hay director de postg agregado al sistema se lanza una excepcion transparente

--- programs/postg_views.py
+++ programs/postg_views.py
@@ -31,11 +31,18 @@ from docx.shared import Inches
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 def index(request):
+
     context={
         'members':PostgMember.objects.all(),
-        'director':PostgMember.objects.get(charge='Director'),
         'programs': Program.objects.all().order_by('-type'),
     }
+    try:
+        director = PostgMember.objects.get(charge='Director')
+        context['director']= director
+
+    except PostgMember.DoesNotExist:
+        pass
+
     return render(request, 'programs/postg/postg_index.html', context)
 
 @login_required
