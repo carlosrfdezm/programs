e3a080630c54a2e6965f7f48d535d42f7bd45a41
Boris Pérez <polux27@gmail.com>
2021-05-14 16:23:53 -0400

    Actualizando cambios para Django 3.2

--- programs/models.py
+++ programs/models.py
@@ -627,7 +627,7 @@ class Requester(models.Model):
     birthdate = models.DateField(null=False)
     theme = models.TextField(max_length=200, null=False)
     request_id = models.CharField(max_length=50, unique=True)
-    request_date = models.DateTimeField(default=now())
+    request_date = models.DateTimeField(default=now)
     planned_end_year = models.IntegerField(null=False)
 
     def __str__(self):
