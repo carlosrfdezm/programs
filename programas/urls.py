162f171b38bd36bd7a64ce81b3d0d9b11f858aa3
Boris Pérez <polux27@gmail.com>
2023-10-03 10:07:41 -0400

    urls

--- programas/urls.py
+++ programas/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-path('password_reset/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='reset_password'),
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('accounts/password_reset/done/', auth_views.PasswordResetDoneView().as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
